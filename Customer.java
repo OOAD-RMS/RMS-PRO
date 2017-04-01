fcae5e63140311cc23938a28fd0c1d2f1ed6d3eb
Ammar288 <smammar.28@gmail.com>
2017-04-01 09:19:28 +0500

    Update Customer.java
    Customer class have empty methods. This class will complete soon.

--- Customer.java
+++ Customer.java
@@ -3,15 +3,48 @@ public class Customer {
 
 	private int mCustomerId;
 	private Order mOrder;
+	private String mCustomerName;
+	
+	public Customer(){
+		
+	}
+	
+	public Customer(int customerId){
+		
+	}
+	
+	public int getCustomerId(){
+		
+	}
 
 	public void setCustomerId(int mCustomerId) {
 		this.mCustomerId = mCustomerId;
 		
 	}
+	
+	public void giveOrder(){
+		
+	}
+	
+	public void iAmDone(){
+
+	}
 
 	public Order getOrder() {
 		
 		return mOrder;
+	}
+	
+	public void setOrder(Order moOrder){
+		
+	}
+	
+	public String getCustomerName(){
+		
+	}
+	
+	public void setCustomerName(){
+		
 	}
 
 }
